076ccbe1a51b203a50b74ae405a2c732a17625b7
unknown <frank.sundstedt@gmail.com>
2020-07-07 21:49:30 -0400

    added to package.json and have basic data showing

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <main>
+      <h2>Global Data</h2>
       <div v-if="typeof data.Global != 'undefined'">
         <div>
           Total confirmed cases: {{ data.Global.TotalConfirmed }}
@@ -8,11 +9,33 @@
         <div>
           Total deaths: {{ data.Global.TotalDeaths }}
         </div>
-                <div>
+        <div>
           Mortality Rate: {{ (data.Global.TotalDeaths/data.Global.TotalConfirmed * 100).toFixed(2) + '%' }}
         </div>
       </div>
         {{ data.Global }}
+      <h2>Data By Country</h2>
+      <div v-if="typeof data.Global != 'undefined'">
+        <div>
+          {{ data.Countries[0]}}
+          <ul>
+            <li v-for='country in data.Countries' :key='country.Country'>
+              <div>
+                {{ country.Country }}
+              </div>
+              <div>
+                Total Cases: {{ country.TotalConfirmed }}
+              </div>
+              <div>
+                Total Deaths: {{ country.TotalDeaths }}
+              </div>
+              <div>
+                Mortality Rate: {{ ( country.TotalDeaths / country.TotalConfirmed * 100 ).toFixed(2) + '%' }}
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </main>
   </div>
 </template>
